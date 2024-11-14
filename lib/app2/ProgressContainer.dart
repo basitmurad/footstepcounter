fbee336dec078b5c75d4e0ce393263da4574192d
basitmurad <basitmurad7@gmail.com>
2024-11-14 22:40:42 +0500

    chnages done

--- lib/app2/ProgressContainer.dart
+++ lib/app2/ProgressContainer.dart
@@ -1,3 +1,65 @@
+// // import 'package:flutter/cupertino.dart';
+// //
+// // class ProgressContainer extends StatelessWidget {
+// //   final String iconPath;
+// //   final String label;
+// //   final String value;
+// //
+// //   ProgressContainer({required this.iconPath, required this.label, required this.value});
+// //
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Column(
+// //       children: [
+// //         SizedBox( height: 8,),
+// //
+// //         Image.asset(
+// //           '',
+// //           width: 28, // Adjust the size as needed
+// //           height: 28, // Adjust the size as needed
+// //         ),
+// //         SizedBox( height: 8,),
+// //
+// //         Text(label),
+// //         SizedBox( height: 4,),
+// //
+// //
+// //         Text(value),
+// //       ],
+// //     );
+// //   }
+// // }
+// import 'package:flutter/cupertino.dart';
+//
+// class ProgressContainer extends StatelessWidget {
+//   final String iconPath;
+//   final String label;
+//   final String value;
+//
+//   ProgressContainer({required this.iconPath, required this.label, required this.value});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         SizedBox( height: 8,),
+//
+//         Image.asset(
+//           '',
+//           width: 28, // Adjust the size as needed
+//           height: 28, // Adjust the size as needed
+//         ),
+//         SizedBox( height: 8,),
+//
+//         Text(label),
+//         SizedBox( height: 4,),
+//
+//
+//         Text(value),
+//       ],
+//     );
+//   }
+// }
 import 'package:flutter/cupertino.dart';
 
 class ProgressContainer extends StatelessWidget {
